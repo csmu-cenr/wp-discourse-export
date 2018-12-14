ac4e853fb9ad7a9e33705450fcaf6dd5fc1d43ef
Keith John Hutchison <keith.hutchison@balance-infosystems.com>
2018-12-14 19:55:23 +1030

    Added extra options for exporting incluing approved, active, throttle and whoa 	new file:   .gitignore 	modified:   admin/admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -67,16 +67,56 @@ class Admin {
 			'export_users_checkbox',
 		), 'wpde_options', 'wpde_settings_section' );
 
+		add_settings_field( 'wpde_exported_users_are_active', __( 'Users are active', 'wpde' ), array(
+			$this,
+			'exported_users_are_active_checkbox',
+		), 'wpde_options', 'wpde_settings_section' );
+
+		add_settings_field( 'wpde_exported_users_are_approved', __( 'Users are approved', 'wpde' ), array(
+			$this,
+			'exported_users_are_approved_checkbox',
+		), 'wpde_options', 'wpde_settings_section' );
+		
+		add_settings_field( 'wpde_export_users_throttle', __( 'Users Throttle', 'wpde' ), array(
+			$this,
+			'export_users_throttle_editbox',
+		), 'wpde_options', 'wpde_settings_section' );
+		
+		add_settings_field( 'wpde_export_users_whoa', __( 'Users Whoa', 'wpde' ), array(
+			$this,
+			'export_users_whoa_editbox',
+		), 'wpde_options', 'wpde_settings_section' );
+		
 		add_settings_field( 'wpds_export_posts', __( 'Export Posts', 'wpde' ), array(
 			$this,
 			'export_posts_checkbox',
 		), 'wpde_options', 'wpde_settings_section' );
 
+		add_settings_field( 'wpde_export_posts_throttle', __( 'Posts Throttle', 'wpde' ), array(
+			$this,
+			'export_posts_throttle_editbox',
+		), 'wpde_options', 'wpde_settings_section' );
+		
+		add_settings_field( 'wpde_export_posts_whoa', __( 'Posts Whoa', 'wpde' ), array(
+			$this,
+			'export_posts_whoa_editbox',
+		), 'wpde_options', 'wpde_settings_section' );
+		
 		add_settings_field( 'wpds_export_comments', __( 'Export Comments', 'wpde' ), array(
 		        $this,
             'export_comments_checkbox',
         ), 'wpde_options', 'wpde_settings_section' );
 
+		add_settings_field( 'wpde_export_posts_throttle', __( 'Posts Throttle', 'wpde' ), array(
+			$this,
+			'export_comments_throttle_editbox',
+		), 'wpde_options', 'wpde_settings_section' );
+		
+		add_settings_field( 'wpde_export_comments_whoa', __( 'Comments Whoa', 'wpde' ), array(
+			$this,
+			'export_comments_whoa_editbox',
+		), 'wpde_options', 'wpde_settings_section' );
+		
 		register_setting( 'wpde_options', 'wpde_options', array( $this->form_helper, 'validate_options' ) );
 
 	}
@@ -105,6 +145,9 @@ class Admin {
 		<a href="?page=wp_discourse_options&tab=wpde_options"
 		   class="nav-tab <?php echo $active ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Export', 'wpde' ); ?>
 		</a>
+		<a href="?page=wp_discourse_options&tab=wpde_options"
+		   class="nav-tab <?php echo $active ? 'nav-tab-active' : ''; ?>"><?php esc_html_e( 'Rate Limits', 'wpde' ); ?>
+		</a>
 		<?php
 	}
 
@@ -121,6 +164,38 @@ class Admin {
 		$this->form_helper->checkbox_input( 'wpde_export_users', 'wpde_options', __( 'Export users.', 'wpde' ) );
 	}
 
+	public function exported_users_are_active_checkbox() {
+		$this->form_helper->checkbox_input( 'wpde_exported_users_are_active', 'wpde_options', __( 'Set users active state when exporting.', 'wpde' ) );
+	}
+
+	public function export_users_throttle_editbox() {
+		$this->form_helper->input( 'wpde_export_users_throttle', 'wpde_options', __( 'The number of milliseconds to wait between calls when exporting users.', 'wpde' ) );
+	}
+	
+	public function export_users_whoa_editbox() {
+		$this->form_helper->input( 'wpde_export_users_whoa', 'wpde_options', __( 'The number of milliseconds to wait on a 429 response.', 'wpde' ) );
+	}
+	
+	public function export_posts_throttle_editbox() {
+		$this->form_helper->input( 'wpde_export_posts_throttle', 'wpde_options', __( 'The number of milliseconds to wait between calls when exporting posts.', 'wpde' ) );
+	}
+	
+	public function export_posts_whoa_editbox() {
+		$this->form_helper->input( 'wpde_export_posts_whoa', 'wpde_options', __( 'The number of milliseconds to wait on a 429 response.', 'wpde' ) );
+	}
+	
+	public function export_comments_throttle_editbox() {
+		$this->form_helper->input( 'wpde_export_comments_throttle', 'wpde_options', __( 'The number of milliseconds to wait between calls when exporting comments.', 'wpde' ) );
+	}
+	
+	public function export_comments_whoa_editbox() {
+		$this->form_helper->input( 'wpde_export_comments_whoa', 'wpde_options', __( 'The number of milliseconds to wait on a 429 response.', 'wpde' ) );
+	}
+	
+	public function exported_users_are_approved_checkbox() {
+		$this->form_helper->checkbox_input( 'wpde_exported_users_are_approved', 'wpde_options', __( 'Set users approved state when importing.', 'wpde' ) );
+	}
+	
 	public function export_posts_checkbox() {
 		$this->form_helper->checkbox_input( 'wpde_export_posts', 'wpde_options', __( 'Export posts.', 'wpde' ) );
 	}
